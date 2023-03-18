e82f7df4dd2040947d3a5df0e8c79da38b57d9d2
bao hua <baohua01234@gmail.com>
2023-03-18 17:35:02 +0700

    Update CI/CD

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing dsadsad &nbsp;
+            Get started by editing &nbsp;
             <code className={styles.code}>Hello everyone</code>
           </p>
           <div>
